0977d531c2b646b219b4b6c6ac97f8173974d225
vincentyang01 <33910245+vincentyang01@users.noreply.github.com>
2021-03-08 14:36:20 -0500

    Time: 68 ms (99.77%), Space: 39.1 MB (84.88%) - LeetHub

--- find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
+++ find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 var sumZero = function(n) {
-    let arr = []
-    let i = 1
-    if(n % 2 === 1){
-        arr.push(0)
-    }
-        
-    while(arr.length != n){
-        arr.push(i)
-        arr.push(i * -1)
-        i++
-    }
-    
-    return arr
+    let arr = []
+    let i = 1
+    if(n % 2 === 1){
+        arr.push(0)
+    }
+        
+    while(arr.length != n){
+        arr.push(i)
+        arr.push(i * -1)
+        i++
+    }
+    
+    return arr
 };
