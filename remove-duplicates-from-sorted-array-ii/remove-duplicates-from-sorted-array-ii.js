0b77f8d1217cd8024ab4c90ff9453a774cdaa857
vincentyang01 <33910245+vincentyang01@users.noreply.github.com>
2021-03-16 15:16:29 -0400

    Time: 92 ms (57.88%), Space: 41 MB (10.58%) - LeetHub

---
+++ remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let obj = {}
+    for(let i = 0; i < nums.length; i++){
+        if(!obj[nums[i]]){
+            obj[nums[i]] = 1
+        } else {
+            obj[nums[i]]++
+        }
+        if(obj[nums[i]] > 2){
+            nums.splice(i, 1)
+            i--
+        }
+    }
+};
